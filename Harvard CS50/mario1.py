be090756ec0798ddff48df323f0f030803aad6d4
lin6echo <bajzathcsaba@gmail.com>
2024-10-17 18:31:26 +0200

    2024-10-17

--- Harvard CS50/mario1.py
+++ Harvard CS50/mario1.py
@@ -10,4 +10,10 @@ for _ in range(3):
 def main():
     print_column(3)
 
+def print_column(height):
+    '''
+    for _ in range(height):
+    print("#")
+    ''' 
+    print("#\n" * height, end="")
 main()
---
+++ Harvard CS50/mario3.py
@@ -0,0 +1,31 @@
+def main():
+    print_square(3)
+
+'''
+def print_square(size):
+
+    # for each row in square
+    for i in range(size):
+
+        # for each brick in row
+        for j in range(size):
+
+            # print brick
+            print("#", end="")
+
+        print()
+'''
+'''
+def print_square(size):
+    for i in range(size):
+        print("#" * size)
+'''
+def print_square(size):
+    for i in range(size):
+        print_row(size)
+
+def print_row(width):
+    print("#" * width)
+
+
+main()
