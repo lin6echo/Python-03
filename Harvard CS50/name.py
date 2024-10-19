b4b8fb3b36344f75813f98243efe6550d1ab991e
lin6echo <bajzathcsaba@gmail.com>
2024-10-19 16:29:08 +0200

    2024-10-19

--- Harvard CS50/name.py
+++ Harvard CS50/name.py
@@ -1,3 +1,26 @@
 import sys
 
+'''
+try:
+    print("hello, my name is", sys.argv[1])
+except IndexError:
+    print("Too few arguments")
+'''
+'''
+# Check for errors
+if len(sys.argv) < 2:
+    print("Too few arguments")
+elif len(sys.argv) > 2:
+    print("Too many arguments")
+
+# Print name tags
+print("hello, my name is", sys.argv[1])
+'''
+
+if len(sys.argv) < 2:
+    sys.exit("Too few arguments")
+
+elif len(sys.argv) > 2:
+    sys.exit("Too many arguments")
+
 print("hello, my name is", sys.argv[1])
