08e591a83daea7b244741ea1e06dd32d72c6a8e2
lin6echo <bajzathcsaba@gmail.com>
2024-10-20 19:46:23 +0200

    2024-10-20

---
+++ Harvard CS50/zzz_saját_03.py
@@ -0,0 +1,14 @@
+def main():
+    count = 0
+    sum = 0
+    x = [77,89,23,45,33]
+    for y in x:
+        sum = sum + y
+        count = count + 1
+    y = sum / count
+
+    print(y)
+        
+
+
+main()
---
+++ Harvard CS50/zzz_saját_04.py
@@ -0,0 +1,10 @@
+x = 31
+y = range(1, x)
+
+for n in y:
+    if x % n == 0:
+        
+        print("Nem prímszám")
+        
+
+    
