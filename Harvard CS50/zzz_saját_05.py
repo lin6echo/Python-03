5d28280f2dd2da7f7b3f1c21df4ad6d2979ff2e8
Bajzáth Csaba <bajzathcsaba@gmail.com>
2024-10-23 13:56:21 +0200

    2024-10-23

--- Harvard CS50/zzz_saját_05.py
+++ Harvard CS50/zzz_saját_05.py
@@ -2,7 +2,11 @@ x = int(input("What's x?"))
 
 
 if x % 2 == 0:
+    x = 2
+    print("It was a prime number")
+elif x % 2 == 0:
     print("This wasn't prime number.")
+
 elif x % 1 == 0 and x % x == 0:
     print("It was prime number")
 
