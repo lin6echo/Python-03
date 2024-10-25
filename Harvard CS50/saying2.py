075024c0eb99f7533258ec6de8c25a6ed4e12537
Bajzáth Csaba <bajzathcsaba@gmail.com>
2024-10-25 10:45:51 +0200

    2024-10-25

--- Harvard CS50/saying2.py
+++ Harvard CS50/saying2.py
@@ -4,3 +4,5 @@ from saying import hello
 
 if len(sys.argv) == 2:
     hello(sys.argv[1])
+
+    
---
+++ Harvard CS50/zzz_saját_06.py
@@ -0,0 +1,15 @@
+x = int(input("What's x?"))
+'''
+i = 1
+
+if x / i > 1:
+    print("x is a prime number")
+elif x / x == 1:
+    print("x is a prime number")
+else:
+    print("x is not a prime number")
+'''
+n = 1
+for n in range(n, n + 1):
+   if  x / n == 1:
+    print(n)
