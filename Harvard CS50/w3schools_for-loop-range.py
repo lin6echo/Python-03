8f82ff972873fa582c0de929f93aa88589aa2be0
lin6echo <bajzathcsaba@gmail.com>
2024-10-25 23:55:27 +0200

    2024-10-25

---
+++ Harvard CS50/w3schools_for-loop-range.py
@@ -0,0 +1,11 @@
+for x in range(6):
+  print(x)
+
+# Using a start parameter
+for x in range(2, 6):
+  print(x)
+
+# Increment the sequence with 3 (default is 1):
+
+for x in range(2, 30, 3):
+  print(x)
--- Harvard CS50/zzz_saját_06.py
+++ Harvard CS50/zzz_saját_06.py
@@ -1,15 +1,7 @@
-x = int(input("What's x?"))
-'''
-i = 1
+x = (input("What's x?"))
 
-if x / i > 1:
-    print("x is a prime number")
-elif x / x == 1:
-    print("x is a prime number")
-else:
-    print("x is not a prime number")
-'''
-n = 1
-for n in range(n, n + 1):
-   if  x / n == 1:
-    print(n)
+i = 1
+for i in x:
+    print(x)
+   
+
