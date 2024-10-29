0396c0b06efa9c7cc73812349a53fe3e9e609222
lin6echo <bajzathcsaba@gmail.com>
2024-10-29 17:42:47 +0100

    2024-10-29

--- Harvard CS50/zzz_saját_06.py
+++ Harvard CS50/zzz_saját_06.py
@@ -1,9 +1,16 @@
-n=int(input("Enter a number: "))
+n = int(input("Enter a number: "))
 
 d = 0
 for i in range(1, n + 1):
     if (n % i) == 0:
         d += 1
         print(i)
+
+if d <= 2:
+        print("This is a prime number.")
+else:
+        print("This is not a prime number")
+
+print("divisors :", d)
 
 
