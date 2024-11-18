4f3e979a8c90552e462ec45ccf89edc22946217a
lin6echo <bajzathcsaba@gmail.com>
2024-11-18 14:16:24 +0100

    204-11-18

---
+++ Python_Full_Course_Free/python_2Dlist-for-loop.py
@@ -0,0 +1,8 @@
+groceries = [["apple", "orange", "banana", "coconut"],
+             ["celery", "carrots", "potatoes"],
+             ["chicken", "fish", "turkey"]]
+
+for collection in groceries:
+    for food in collection:  # nested loop
+        print(food, end=" ")
+    print()
---
+++ Python_Full_Course_Free/python_2Dlist.py
@@ -0,0 +1,9 @@
+# 2dlist = [liar1, list2, list3]
+
+fruits =     ["apple", "orange", "banana, coconut"]
+vegetables = ["celery", "carrots", "potatoes"]
+meats =      ["chicken", "fish", "turkey"]
+
+groceries = [fruits, vegetables, meats]
+
+print(groceries[0][0])
