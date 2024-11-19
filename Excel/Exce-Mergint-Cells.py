c5dbdca0d4f39568bda2036bca85a23f5870416e
lin6echo <bajzathcsaba@gmail.com>
2024-11-19 12:37:13 +0100

    2024-11-19

---
+++ Excel/Exce-Mergint-Cells.py
@@ -0,0 +1,13 @@
+import openpyxl
+wb = openpyxl.Workbook()
+sheet = wb.active
+
+sheet.merge_cells("A2:D4")
+
+sheet.cell(row=2, column=1).value = "Twelve cells join together."
+
+# merge cell C6 and D6
+
+sheet.cell(row=6, column=6).value = "Two merge cells"
+
+wb.save("Python-03/Excel/sample2.xlsx")
--- Excel/Excel-Write.py
+++ Excel/Excel-Write.py
@@ -1,3 +1,4 @@
+# import openpyxl module
 import openpyxl
 
 wb = openpyxl.Workbook()
@@ -5,6 +6,7 @@ wb = openpyxl.Workbook()
 sheet = wb.active
 
 c1 = sheet.cell(row=1, column=1)
+# writing values to cells
 c1.value = "Hello"
 
 c2 = sheet.cell(row=1, column=2)
@@ -13,6 +15,7 @@ c2.value = "World"
 c3 = sheet["A2"]
 c3.value = "Welcome"
 
+# B2 means column = 2 & row = 2.
 c4 = sheet["B2"]
 c4.value = "Everyone"
 
