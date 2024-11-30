12f0ba7f94001f53f5b076883ebc852c7a7c426f
lin6echo <bajzathcsaba@gmail.com>
2024-11-30 17:05:06 +0100

    2024-11-30

---
+++ Python_Full_Course_Free/0068_match-case-statement.py
@@ -0,0 +1,35 @@
+# Match-case statement (switch): An alternative to using many "elif" statements
+#                                Execute some code if a value matches a "case"
+#                                Benefits: cleaner and syntax is more readable
+'''
+def is_weekend(day):
+    match day:
+        case "Sunday":
+            return True
+        case "Monday":
+            return False
+        case "Tuesday":
+            return False
+        case "Wednesday":
+            return False
+        case "Thursday":
+            return False
+        case "Friday":
+            return False
+        case "Saturday":
+            return True
+        case _:
+            return False
+        
+print(is_weekend("Monday"))
+'''
+def is_weekend(day):
+    match day:
+        case "Saturday" | "Sunday":
+            return True
+        case "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday":
+            return False
+        case _:
+            return False
+        
+print(is_weekend("Monday"))
